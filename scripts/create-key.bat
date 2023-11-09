653b20e9fd31456cd282e314d1de355618bd8588
Jacobtread <jacobtread@gmail.com>
2023-11-09 15:37:37 +1300

    Fixed script and updated keys

--- scripts/create-key.bat
+++ scripts/create-key.bat
@@ -1,21 +1,39 @@
 @rem Credit to https://github.com/Aim4kill/Bug_OldProtoSSL 
-
 @echo off
 
-rem Create CA
-openssl req -nodes -new -md5 -x509 -days 28124 -keyout ca-key.pem -out ca-cert.crt -subj "/OU=Online Technology Group/O=Electronic Arts, Inc./L=Redwood City/ST=California/C=US/CN=OTG3 Certificate Authority"
 
-rem Create certificate request and key
-openssl req -nodes -new -keyout key.pem -out cert.csr -subj "/CN=gosredirector.ea.com/OU=Global Online Studio/O=Electronic Arts, Inc./ST=California/C=US"
+rem Certificate Authority name
+set CA_NAME=OTG3
+
+rem Certificate name
+set C_NAME=gosredirector
+
+rem Modified der file name, that will be used later
+set MOD_NAME=gosredirector_mod
+
+
+rem Create private key for the Certificate Authority
+openssl genrsa -aes128 -out %CA_NAME%.key.pem -passout pass:123456 1024
+openssl rsa -in %CA_NAME%.key.pem -out %CA_NAME%.key.pem -passin pass:123456
+
+rem Create the certificate of the Certificate Authority
+openssl req -new -md5 -x509 -days 28124 -key %CA_NAME%.key.pem -out %CA_NAME%.crt -subj "/OU=Online Technology Group/O=Electronic Arts, Inc./L=Redwood City/ST=California/C=US/CN=OTG3 Certificate Authority"
+
+rem ------------Certificate Authority created, now we can create Certificate------------
+
+rem Create private key for the Certificate
+openssl genrsa -aes128 -out %C_NAME%.key.pem -passout pass:123456 1024
+openssl rsa -in %C_NAME%.key.pem -out %C_NAME%.key.pem -passin pass:123456
+
+rem Create certificate signing request of the certificate
+openssl req -new -key %C_NAME%.key.pem -out %C_NAME%.csr -subj "/CN=gosredirector.ea.com/OU=Global Online Studio/O=Electronic Arts, Inc./ST=California/C=US"
 
 rem Create the certificate
-openssl x509 -req -in cert.csr -CA ca-cert.crt -CAkey ca-key.pem -outform der -out cert.der -days 10000 -md5
+openssl x509 -req -in %C_NAME%.csr -CA %CA_NAME%.crt -CAkey %CA_NAME%.key.pem -CAcreateserial -out %C_NAME%.crt -days 10000 -md5
 
-rem Remove completed certificate request file
-del cert.csr
+rem ------------Certificate created, now export it to .der format so we can modify it------------
+openssl x509 -outform der -in %C_NAME%.crt -out %C_NAME%.der
 
-rem Remove unused ca files (Comment these lines if you want them)
-del ca-key.pem
-del ca-cert.crt
+echo Der file exported, now patch it manually
+pause
 
-rem Modify the cert.der file to match
